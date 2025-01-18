68ca28b0e2af9f5b35aad0c960ede39a18149b8c
zldnlto <langchomi0@gmail.com>
2025-01-19 01:38:21 +0900

    Chore: @nestjs/config 설치 후 Prisma Client 설정

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats.module';
+import { PrismaService } from '../prisma/prisma.service';
+import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 앱 어디서나 환경 변수를 사용할 수 있도록 설정
+    }),
+    CatsModule,
+    PostsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
