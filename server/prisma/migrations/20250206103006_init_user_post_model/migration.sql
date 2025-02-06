92bafce986c89f687b44adcc6f8fa219b75e7470
zldnlto <langchomi0@gmail.com>
2025-02-06 19:32:42 +0900

    Feat: Prisma migration 파일 추가 데이터 모델 변경 사항 반영 (Post, User 모델 init)

---
+++ server/prisma/migrations/20250206103006_init_user_post_model/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `Post` table. All the data in the column will be lost.
+  - Added the required column `authorId` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "userId",
+ADD COLUMN     "authorId" INTEGER NOT NULL,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
