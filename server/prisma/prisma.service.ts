c4bf4887fcb51de512586c26b41a22a66a287cf7
zldnlto <langchomi0@gmail.com>
2025-02-07 16:01:41 +0900

    Feat: PrismaService 초기화 및 종료 로직 추가 외 트랜잭션 관리 및 로깅 미들웨어 설정 포함

--- server/prisma/prisma.service.ts
+++ server/prisma/prisma.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
-    super();
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+
+    // 모든 쿼리에 대한 로깅 미들웨어
+    this.$use(async (params, next) => {
+      console.log(`✨실행: ${params.model}.${params.action}`);
+      return next(params);
+    });
+  }
+
+  // 모듈이 초기화될 때 DB 연결
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  // 모듈이 종료될 때 DB 연결 해제
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
