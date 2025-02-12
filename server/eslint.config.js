e9c4e54da760cedeff879c27bb9b4cc28cde2bce
zldnlto <langchomi0@gmail.com>
2025-02-13 02:36:29 +0900

    Chore: ESLint config ->  eslint.config.js 마이그레이션 - .eslintrc.js 파일을 eslint.config.js로 마이그레이션하여 최신 ESLint v9+ 방식에 맞게 수정 - simple-import-sort 플러그인 사용하여 import 순서 자동 정렬되도록 설정

---
+++ server/eslint.config.js
@@ -0,0 +1,46 @@
+// eslint.config.js
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+export default [
+  js.configs.recommended,
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  {
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'prettier/prettier': 'warn',
+    },
+    settings: {
+      env: {
+        node: true,
+        jest: true,
+      },
+    },
+  },
+  prettierConfig,
+];
