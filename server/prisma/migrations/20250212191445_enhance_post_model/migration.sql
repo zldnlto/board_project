e3206dc32bad7354ee01e5dede152811fea30b02
zldnlto <langchomi0@gmail.com>
2025-02-13 04:15:54 +0900

    Feat: Post 모델 개선 및 Prisma 마이그레이션 적용 - Post 모델 개선 (title 제한, content 타입 변경, category 필드 추가)

---
+++ server/prisma/migrations/20250212191445_enhance_post_model/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `title` on the `Post` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "category" TEXT,
+ALTER COLUMN "title" SET DATA TYPE VARCHAR(255);
