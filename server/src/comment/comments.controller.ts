eadc4506d1fbbf21fda89258ef16f62ade8a24cf
zldnlto <langchomi0@gmail.com>
2025-02-14 01:02:31 +0900

    Feat: Comment 도메인의 기본 구조 생성

---
+++ server/src/comment/comments.controller.ts
@@ -0,0 +1,9 @@
+import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CommentsService } from './comments.service';
+
+@ApiTags('comments')
+@Controller('comments')
+export class CommentsController {
+  constructor(private readonly postsService: CommentsService) {}
+}
